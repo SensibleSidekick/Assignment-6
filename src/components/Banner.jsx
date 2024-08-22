9ab42b41b13e2e0300a0b6e99b73ca4991b4f3dd
Kelly Doherty <kad2321@yahoo.com>
2024-08-22 15:11:26 -0400

    Working on Table, adjusted Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import classes from './styling.css';
 
-class Banner extends React.Component {
-    render() {
+const Banner = () => {
         return (
          <div>
             <header>Orbit Report</header>
             <p>Please click on the buttons to see the satellites in that orbit type.</p>
          </div>
         );
-    }
 }
 
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,29 @@
+
+
 const Table = ({ sat }) => {
+  let listItem = sat.map((sat, id) =>
+ 
+    <tr key={sat.id}>
+      <td>{sat.name}</td>
+      <td>{sat.type}</td>
+      <td>{sat.launchDate}</td>
+      <td>{sat.operational === 'true' ? "Active" : "Inactive"}</td>
+    </tr>
+
+  )
+  
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      {listItem}
      </tbody>
    </table>
   );
